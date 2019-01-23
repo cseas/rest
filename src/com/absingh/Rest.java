cdefb781aa496ef1e7fac1c59714eccedf96af9a
cseas <contact.abhijeetsingh@gmail.com>
2019-01-24 00:23:47 +0530

    Initialize project and add code

---
+++ src/com/absingh/Rest.java
@@ -0,0 +1,35 @@
+package com.absingh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class Rest {
+
+    public static void main(String[] args) throws IOException {
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            HttpGet request = new HttpGet("http://services.groupkt.com/state/get/IND/UP");
+            HttpResponse response = client.execute(request);
+
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+
+            while ((line = bufReader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+            }
+
+            System.out.println(builder);
+        }
+    }
+}
